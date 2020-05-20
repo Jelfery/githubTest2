a4f4a8b3ffcb48a7f1bc0440625f1fa94189a5b4
Jelfery <jedaemric@gmail.com>
2020-05-21 07:50:55 +0800

    Update main.dart
    home change to welcome

--- githubtest2/lib/main.dart
+++ githubtest2/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Testing Github'),
+      home: MyHomePage(title: 'Welcome to githubtest2'),
     );
   }
 }
